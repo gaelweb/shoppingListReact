b7a4160985c62c31844fc67098ad49c5e2fa2821
gael monbrault <gael.monbrault@ovianet.com>
2021-01-06 16:26:10 +0100

    update Router config in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
-import ShoppingList from './components/ShoppingList/ShoppingList.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Navbar from './components/Navbar/Navbar.jsx';
+import ShopList from './views/ShopList.jsx';
+import Spend from './components/Spend/Spend.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <ShoppingList />
-    </div>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+              <Spend />
+            </Route>
+            <Route path="/shoppingList">
+              <ShopList />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
   );
+
+  function Home(){
+    return <h2>Home</h2>
+  }
 }
 
 export default App;
